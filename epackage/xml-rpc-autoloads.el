ecd88edb8fd25cbe03891188e6e15ed2b530fed9
Jari Aalto <jari.aalto@cante.net>
2011-04-11 10:21:55 +0300

    epackage/xml-rpc-autoloads.el: new

---
+++ epackage/xml-rpc-autoloads.el
@@ -0,0 +1,28 @@
+(autoload 'xml-rpc-value-intp                   "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-value-doublep                "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-value-stringp                "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-value-structp                "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-value-arrayp                 "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-submit-bug-report            "xml-rpc" "" t)
+(autoload 'xml-rpc-value-booleanp               "xml-rpc" "" nil)
+(autoload 'xml-rpc-value-datetimep              "xml-rpc" "" nil)
+(autoload 'xml-rpc-string-to-boolean            "xml-rpc" "" nil)
+(autoload 'xml-rpc-caddar-safe                  "xml-rpc" "" nil)
+(autoload 'xml-rpc-xml-list-to-value            "xml-rpc" "" nil)
+(autoload 'xml-rpc-boolean-to-string            "xml-rpc" "" nil)
+(autoload 'xml-rpc-datetime-to-string           "xml-rpc" "" nil)
+(autoload 'xml-rpc-value-to-xml-list            "xml-rpc" "" nil)
+(autoload 'xml-rpc-xml-to-string                "xml-rpc" "" nil)
+(autoload 'xml-rpc-response-errorp              "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-response-error-code          "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-response-error-string        "xml-rpc" "" nil);;defsubst
+(autoload 'xml-rpc-xml-to-response              "xml-rpc" "" nil)
+(autoload 'xml-rpc-request                      "xml-rpc" "" nil)
+(autoload 'xml-rpc-clean-string                 "xml-rpc" "" nil)
+(autoload 'xml-rpc-clean                        "xml-rpc" "" nil)
+(autoload 'xml-rpc-request-process-buffer       "xml-rpc" "" nil)
+(autoload 'xml-rpc-request-callback-handler     "xml-rpc" "" nil)
+(autoload 'xml-rpc-method-call-async            "xml-rpc" "" nil)
+(autoload 'xml-rpc-method-call                  "xml-rpc" "" nil)
+
+(provide 'xml-rpc-autoloads)
